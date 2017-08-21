57e9ff9e32a11bb95c4592c1bd9c8a1dba663644
dengjun <763124572@qq.com>
2017-08-22 00:06:10 +0800

    修改数据

---
+++ jsmenu/Tmenu/pages/finishOrder/finishOrder.js
@@ -0,0 +1,30 @@
+// pages/finishOrder/finishOrder.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+  //点击调戏好友
+  formSubmit:function(e){
+        console.log(e.detail.value);
+        wx.navigateTo({
+            url: '../transmitHB/transmitHB'
+        })
+  },
+  //点击去菜单
+  gotoMenu:function(){
+      wx.navigateTo({
+          url: '../menu/menu'
+      })
+  }
+})

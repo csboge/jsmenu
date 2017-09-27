e161b8168085afd77bbff0836b24f4543f83aa58
dengjun <763124572@qq.com>
2017-09-27 09:58:35 +0800

    2.0

--- Teacher/pages/index/index.js
+++ Teacher/pages/index/index.js
@@ -4,7 +4,13 @@ const app = getApp()
 
 Page({
     data: {
-        
+        imgUrls: [
+            "http://img.my-shop.cc/teacher/tc_slide1.jpg",
+            "http://img.my-shop.cc/teacher/tc_slide2.jpg",
+            "http://img.my-shop.cc/teacher/tc_slide3.jpg",
+            "http://img.my-shop.cc/teacher/tc_slide4.jpg",
+            "http://img.my-shop.cc/teacher/tc_slide5.jpg"
+        ]
     },
     onLoad: function () {
 

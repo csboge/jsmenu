dd5d92b4becec7ba7ed76be884b50f6f705c2f9e
dengjun <763124572@qq.com>
2017-09-27 13:09:42 +0800

    2.0.2

--- Teacher/app.js
+++ Teacher/app.js
@@ -4,10 +4,19 @@ App({
         // 展示本地存储能力
         var logs = wx.getStorageSync('logs') || []
         logs.unshift(Date.now())
-        wx.setStorageSync('logs', logs)
+        wx.setStorageSync('logs', logs);
 
     },
+    onShow() {
+        let that = this;
+
+        wx.getNetworkType({
+            success: function (res) {
+                that.globalData.networkType = res.networkType;
+            }
+        });
+    },
     globalData: {
-        
+
     }
 })
--- Teacher/pages/index/index.js
+++ Teacher/pages/index/index.js
@@ -10,13 +10,23 @@ Page({
             "http://img.my-shop.cc/teacher/tc_slide3.jpg",
             "http://img.my-shop.cc/teacher/tc_slide4.jpg",
             "http://img.my-shop.cc/teacher/tc_slide5.jpg"
-        ]
+        ],
+        auto_play: false
     },
     onLoad: function () {
 
+
+    },
+    onReady() {
+        let that = this;
+
+        if (app.globalData.networkType == "wifi") {
+            let videoContext = wx.createVideoContext('myVideo');
+            videoContext.play();
+        }
     },
     //拨打电话
-    call(){
+    call() {
         wx.makePhoneCall({
             phoneNumber: '18671621319'
         });
